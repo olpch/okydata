5338e31ad597e3856e2c44ec7dbcfa304acecd23
Omar Lee <omarleeace@gmail.com>
2014-11-30 19:58:59 -0500

    ultimo commit 30 nov 2014 - inventarios view commit

---
+++ models/itemModel.php
@@ -0,0 +1,19 @@
+<?php
+
+class UserModel extends Model{
+
+	public function __construct(){
+			parent::__construct();
+	}
+	
+	public function all($numidex = false){
+		$c  = 'SELECT i.id, i.name, i.state, i.tag';
+		$c .= 'FROM  itemi';
+		$res = $this->_db->query($c);
+		if ($numidex){
+			return $res->fetchAll(PDO::FETCH_NUM);
+		}else{
+			return $res->fetchAll(PDO::FETCH_ASSOC);
+		}
+	}
+}
